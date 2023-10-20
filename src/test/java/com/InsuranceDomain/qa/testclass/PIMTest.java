cf9be6dcdd94c74d7bb57d878ffc2a750db592b2
vikasjagtapjv <vikasjagtap8999@gmail.com>
2023-10-20 17:40:59 +0530

    PIM test updated

--- src/test/java/com/InsuranceDomain/qa/testclass/PIMTest.java
+++ src/test/java/com/InsuranceDomain/qa/testclass/PIMTest.java
@@ -12,7 +12,27 @@ public class PIMTest extends BaseClass {
 		PIMPom pp=new PIMPom(driver);
 		
 		// -----SEARCH EMPLOYEE WITH AVAILABLE INFORMATION -------//
-
-	}
+		pp.click_OnPim();
+		pp.setEmployeeNm(pp.getEmployeeNm());
+		pp.clickOnEmp();
+		pp.setEmpID(pp.getEmpID());
+		pp.clickOn_Save();
+		
+		//--ADD EMPLOYEE----------------------//
+		pp.click_On_addEmp();
+		pp.setEmployeefn("scott");
+		pp.setEmployeeMN("edword");
+		pp.setEmployeeLN("decort");
+		pp.setEmployeeID("0698");
+		pp.clickOnSave();
+		
+		//---REPORTS--------------
+		pp.enterOnReport();
+		pp.setReportName(pp.getReportName());
+		pp.enterOnHint();
+		pp.click_On_Search();
+		pp.clickON_Report();
+		
+		}
 
 }
